2251953b3a5eeb2b480cbefc88adea418815d961
nh <nickxhan@umich.edu>
2025-08-15 23:45:45 -0400

    Add dialog + form, which are partially working
    Submit button broke at some point, and I still
    haven't implemented actually obtaining the form's
    values.

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-function Book(title, author, pages, hasRead) {
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
     if (!new.target) {
         throw Error("NO");
     }
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.hasRead = hasRead;
+    this.read = read;
     this.info = function() {
-        return `${this.title} by ${this.author}, ${pages} pages, ` + (this.hasRead === false ? "not read yet" : "already read");
+        return `${this.title} by ${this.author}, ${pages} pages, ` + (this.read === false ? "not read yet" : "already read");
     }
 }
 
-let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 255, false);
-console.log(theHobbit.info());
+function addBookToLibrary(library, title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    library.push(book);
+}
+
+const display = document.querySelector(".display");
+const modal = document.querySelector("#form-modal");
+const addButton = document.querySelector(".add");
+const closeButton = document.querySelector(".close");
+const submitButton = document.querySelector(".submit");
+
+myLibrary.forEach(book => {
+    const div = document.createElement("div");
+    div.classList.add("book");
+    div.textContent = book.info();
+    display.appendChild(div);
+})
+
+addButton.addEventListener("click", e => {
+    modal.showModal();
+})
+
+closeButton.addEventListener("click", e => {
+    modal.close();
+})
+
+submitButton.addEventListener("click", e => {
+    e.preventDefault();
+    e.close();
+})

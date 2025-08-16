9903ceccb548422d638e1e42463e3150728ee836
nh <nickxhan@umich.edu>
2025-08-16 00:31:25 -0400

    Styled checkbox for having read the book
    Used MDN Advanced Styling guide for that, as
    checkboxes are one of the more difficult form
    elements to style.

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(library, title, author, pages, read) {
     let book = new Book(title, author, pages, read);
     library.push(book);
+    const div = document.createElement("div");
+    div.classList.add("book");
+    div.textContent = book.info();
+    display.appendChild(div);
 }
 
 const display = document.querySelector(".display");
@@ -24,14 +28,20 @@ const modal = document.querySelector("#form-modal");
 const addButton = document.querySelector(".add");
 const closeButton = document.querySelector(".close");
 const submitButton = document.querySelector(".submit");
-const form = document.querySelector("form");
 
-myLibrary.forEach(book => {
-    const div = document.createElement("div");
-    div.classList.add("book");
-    div.textContent = book.info();
-    display.appendChild(div);
-})
+//form
+const form = document.querySelector("form");
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const isRead = document.querySelector('#read');
+
+// myLibrary.forEach(book => {
+//     const div = document.createElement("div");
+//     div.classList.add("book");
+//     div.textContent = book.info();
+//     display.appendChild(div);
+// })
 
 addButton.addEventListener("click", e => {
     modal.showModal();
@@ -41,12 +51,10 @@ closeButton.addEventListener("click", e => {
     modal.close();
 })
 
-submitButton.addEventListener("click", e => {
-    // e.preventDefault();
-    
-    // modal.close();
-})
+form.addEventListener("submit", e => {
+    e.preventDefault();
 
-form.addEventListener("formdata", e => {
-    console.log(e.formData.getAll());
+    addBookToLibrary(myLibrary, title.value, author.value, pages.value, isRead.value);
+
+    modal.close();
 })
